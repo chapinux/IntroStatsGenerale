108211777e5d46c8f1b3af80f3b36c12967b5c31
paul c <paul.chapron@gmail.com>
2021-12-05 15:53:56 +0100

    version latex de l'intro

---
+++ script_figures.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+ggplot(data = penguins, 
+       aes(x = flipper_length_mm,
+           y = body_mass_g)) +
+  geom_point(aes(color = species, 
+                 shape = species),
+             size = 3,
+             alpha = 0.8) +
+  #theme_minimal() +
+  scale_color_manual(values = c("darkorange","purple","cyan4")) +
+  labs(title = "Penguin size, Palmer Station LTER",
+       subtitle = "Flipper length and body mass for Adelie, Chinstrap and Gentoo Penguins",
+       x = "Flipper length (mm)",
+       y = "Body mass (g)",
+       color = "Penguin species",
+       shape = "Penguin species") +
+  stat_smooth(method="lm", se=FALSE)+
+  theme_minimal()
